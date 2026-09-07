8be66eef7fba2b57022cec60b6d2cbf24db082e5
durjoyh335-dot <durjoyh335@gmail.com>
2026-09-07 20:21:03 +0600

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -1,21 +1,47 @@
 package com.example
 
-import com.lagradost.cloudstream3.MainAPI
-import com.lagradost.cloudstream3.SearchResponse
-import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
 
-class ExampleProvider : MainAPI() { // All providers must be an instance of MainAPI
-    override var mainUrl = "https://example.com/" 
-    override var name = "Example provider"
-    override val supportedTypes = setOf(TvType.Movie)
-
-    override var lang = "en"
-
-    // Enable this when your provider has a main page
+class HiAnimeProvider : MainAPI() {
+    override var mainUrl = "https://hianime.to"
+    override var name = "HiAnime"
     override val hasMainPage = true
+    override val supportedTypes = setOf(TvType.Anime, TvType.AnimeMovie)
 
-    // This function gets called when you search for something
+    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse? {
+        val document = app.get(mainUrl).document
+        val homeItems = ArrayList<HomePageList>()
+        
+        val items = document.select("div.flw-item")
+        val animeList = items.mapNotNull { 
+            val title = it.selectFirst("h3.film-name a")?.text() ?: return@mapNotNull null
+            val href = it.selectFirst("h3.film-name a")?.attr("href") ?: return@mapNotNull null
+            val poster = it.selectFirst("img.film-poster-img")?.attr("data-src")
+            
+            newAnimeSearchResponse(title, fixUrl(href), TvType.Anime) {
+                this.posterUrl = poster
+            }
+        }
+        
+        if (animeList.isNotEmpty()) {
+            homeItems.add(HomePageList("Trending Anime", animeList))
+        }
+        return HomePageResponse(homeItems)
+    }
+
     override suspend fun search(query: String): List<SearchResponse> {
-        return listOf()
+        val searchUrl = "$mainUrl/search?keyword=$query"
+        val document = app.get(searchUrl).document
+        
+        return document.select("div.flw-item").mapNotNull {
+            val title = it.selectFirst("h3.film-name a")?.text() ?: return@mapNotNull null
+            val href = it.selectFirst("h3.film-name a")?.attr("href") ?: return@mapNotNull null
+            val poster = it.selectFirst("img.film-poster-img")?.attr("data-src")
+            
+            newAnimeSearchResponse(title, fixUrl(href), TvType.Anime) {
+                this.posterUrl = poster
+            }
+        }
     }
 }
